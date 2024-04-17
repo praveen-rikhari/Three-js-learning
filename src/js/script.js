a54fed6ee3996c469cb8a11bbbb1a22d8b65269c
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-18 01:09:44 +0530

    adding more shapes and grids

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ const scene = new THREE.Scene();
 
 // adding perspective camera
 const camera = new THREE.PerspectiveCamera(
-    75,     // field of view
+    45,     // field of view
     window.innerWidth / window.innerHeight,     // aspect ratio
     // near & far clipping plane
     0.1,
@@ -33,7 +33,7 @@ const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 //changing camera position w.r.t. z-axis and y-axis
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
 
 // we need to update then scene after everytime we change the camera postions
 orbit.update();
@@ -48,6 +48,33 @@ const boxMaterial = new THREE.MeshBasicMaterial(
 );
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
+
+// adding plane
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+
+    // this propewrty adds view to all the sides
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+//adding grid helper
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+// adding sphere
+const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    // adds and removes wireframe from 3d object
+    wireframe: true
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+sphere.position.set(-10, 10, 0);
 
 //adding animation
 function animate(time) {
