a033b9e75ee02ef35b2e368dbfe1976a52e6b4c8
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-17 21:32:36 +0530

    creating scene & adding camera

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,33 @@ import * as THREE from 'three';
 const renderer = new THREE.WebGLRenderer();
 
 //setting size of that space
-renderer.setSize(window.innerHeight, window.innerWidth);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 //injecting that space(which is basically a canvas element)
 document.body.appendChild(renderer.domElement);
+
+//creating scene
+const scene = new THREE.Scene();
+
+// adding perspective camera
+const camera = new THREE.PerspectiveCamera(
+    75,     // field of view
+    window.innerWidth / window.innerHeight,     // aspect ratio
+    // near & far clipping plane
+    0.1,
+    1000
+);
+
+// adding axeshelper to guide through coordinates axes
+// 5 here represents the length of the axes
+const axesHelper = new THREE.AxesHelper(5);
+
+//adding axesHelper to scene
+scene.add(axesHelper);
+
+//changing camera position w.r.t. z-axis and y-axis
+camera.position.set(0,2,5)
+
+
+// linking scene with camera
+renderer.render(scene, camera);
